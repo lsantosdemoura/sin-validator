ac94b6ba4ca0b9331b5acc44b1ce647cfa68de1c
Lucas Moura <lsantosdemoura99@gmail.com>
2024-10-28 18:32:02 -0300

    fix: luhn algorithm checking second digit.

--- src/useCases/luhnAlgorithm.ts
+++ src/useCases/luhnAlgorithm.ts
@@ -1,9 +1,13 @@
-export function calculateLuhnAlgorithm(sin: string): boolean {
+export function calculateLuhnAlgorithm(code: string): boolean {
   let sum = 0
-  for (let i = sin.length - 1; i >= 0; i--) {
-    let digit = parseInt(sin[i])
-    if (i % 2 === 1) digit *= 2
-    if (digit > 9) digit -= 9
+  let isSecondDigit = false
+  for (let i = code.length - 1; i >= 0; i--) {
+    let digit = parseInt(code[i])
+    if (isSecondDigit) {
+      digit *= 2
+      if (digit > 9) digit -= 9
+    }
+    isSecondDigit = !isSecondDigit
     sum += digit
   }
   return sum % 10 === 0
